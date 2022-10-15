b756fced83acf81f25ac4dfcaac00bb5dac39db7
zK3lvin <109186535+zK3lvin@users.noreply.github.com>
2022-10-15 15:39:54 -0500

    update Loader.php

--- src/Fly/Loader.php
+++ src/Fly/Loader.php
@@ -1,1 +1,14 @@
+<?php
 
+namespace Fly;
+
+use pocketmine\plugin\PluginBase;
+use Fly\command\FlyCommand;
+
+class Loader extends PluginBase {
+
+    public function onEnable(): void {
+        $this->getLogger()->info("Plugin activado by zk3lvin.");
+        $this->getServer()->getCommandMap()->register("/fly", new FlyCommand($this));
+    }
+}
